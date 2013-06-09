bcfadbb65f3c3e406a3f4e6bbbc393f0ced56204
Evan Jones <ej@evanjones.ca>
2013-06-09 15:30:29 -0400

    streak_example.go: Remove stray trailing : from output lines

--- cmd/streak_example.go
+++ cmd/streak_example.go
@@ -21,8 +21,8 @@ func main() {
 	}
 	for _, pipeline := range pipelines {
 		fmt.Printf("  %s:\n", pipeline.Name)
-		fmt.Printf("  .Key: %s:\n", pipeline.Key)
-		fmt.Printf("  .Description: %s:\n\n", pipeline.Description)
+		fmt.Printf("  .Key: %s\n", pipeline.Key)
+		fmt.Printf("  .Description: %s\n\n", pipeline.Description)
 	}
 
 	fmt.Printf("Boxes in pipeline \"%s\":\n", pipelines[0].Name)
@@ -33,9 +33,9 @@ func main() {
 	var gmailThreadBox *streak.Box
 	for i, box := range boxes {
 		fmt.Printf("  %s:\n", box.Name)
-		fmt.Printf("  .Key: %s:\n", box.Key)
-		fmt.Printf("  .LastUpdatedTimestamp: %v:\n", streak.TimestampToTime(box.LastUpdatedTimestamp))
-		fmt.Printf("  .GmailThreadCount: %d:\n\n", box.GmailThreadCount)
+		fmt.Printf("  .Key: %s\n", box.Key)
+		fmt.Printf("  .LastUpdatedTimestamp: %v\n", streak.TimestampToTime(box.LastUpdatedTimestamp))
+		fmt.Printf("  .GmailThreadCount: %d\n\n", box.GmailThreadCount)
 		if gmailThreadBox == nil && box.GmailThreadCount > 0 {
 			gmailThreadBox = &boxes[i]
 			fmt.Println("wtf?", gmailThreadBox)
